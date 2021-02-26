d15ff0dbc445d99dbbbafa510973521f0e11d659
evelyn655 <evelyn108061124@gapp.nthu.edu.tw>
2021-02-26 02:16:24 -0800

    add led.cpp

--- mbed-os-test-program3/led.cpp
+++ mbed-os-test-program3/led.cpp
@@ -5,7 +5,7 @@ void Led1(DigitalOut &ledpin)
     for (int i=0; i<6; ++i)
     {                       // blink for 3 times
         ledpin = !ledpin;   // toggle led
-        ThisThread::sleep_for(100ms);
+        ThisThread::sleep_for(500ms);
     }
 }
 
@@ -14,6 +14,6 @@ void Led2(DigitalOut &ledpin)
     for (int i=0; i<4; ++i)
     {                       // blink for 2 times
         ledpin = !ledpin;   // toggle led
-        ThisThread::sleep_for(100ms);
+        ThisThread::sleep_for(500ms);
     }
 }
